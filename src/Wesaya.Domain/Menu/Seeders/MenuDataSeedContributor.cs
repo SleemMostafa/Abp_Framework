ff0f41a8599a0db4a3fbe81b395fdacf605302e5
Sleem Mostafa <sleemmostafa1995@gmail.com>
2026-06-06 18:26:01 +0300

    feat(menu): implement MenuDataSeedContributor and SeedExtraItem for data seeding

---
+++ src/Wesaya.Domain/Menu/Seeders/MenuDataSeedContributor.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Uow;
+
+namespace Wesaya.Menu;
+
+public class MenuDataSeedContributor(
+    MenuCategoryDataSeeder categoryDataSeeder,
+    MenuItemDataSeeder itemDataSeeder)
+    : IDataSeedContributor, ITransientDependency
+{
+    public ILogger<MenuDataSeedContributor> Logger { get; set; } = NullLogger<MenuDataSeedContributor>.Instance;
+
+    [UnitOfWork]
+    public virtual async Task SeedAsync(DataSeedContext context)
+    {
+        Logger.LogInformation("Started menu seed.");
+
+        await categoryDataSeeder.SeedAsync();
+        await itemDataSeeder.SeedAsync();
+
+        Logger.LogInformation("Finished menu seed.");
+    }
+}
